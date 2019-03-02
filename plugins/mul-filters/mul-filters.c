41901b6b520a802bc3424c36d1333812abef0d0f
Roman Dobias <rom.dobias@gmail.com>
2019-03-03 00:09:40 +0100

    WIP on chromatic aberrations

--- plugins/mul-filters/mul-filters.c
+++ plugins/mul-filters/mul-filters.c
@@ -10,6 +10,7 @@ MODULE_EXPORT const char *obs_module_description(void)
 
 extern struct obs_source_info mirror_filter;
 extern struct obs_source_info color_inverse_filter;
+extern struct obs_source_info chromatic_aberration_filter;
 extern struct obs_source_info radial_wave_filter;
 extern struct obs_source_info pixelize_filter;
 extern struct obs_source_info gauss_filter;
@@ -19,6 +20,7 @@ bool obs_module_load(void)
 {
 	obs_register_source(&mirror_filter);
 	obs_register_source(&color_inverse_filter);
+	obs_register_source(&chromatic_aberration_filter);
 	obs_register_source(&radial_wave_filter);
 	obs_register_source(&pixelize_filter);
 	obs_register_source(&gauss_filter);
